373697b371ac0773b75c1f8951906157c01a188d
Mohammed Sami <mshelal555@gmail.com>
2022-10-04 23:24:50 +0200

    create project back-end

---
+++ back end/src/test/java/com/spring/restuarant/RestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.restuarant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
