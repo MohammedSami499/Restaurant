b17f8b57f5ae76730d7cc48b9bbd11513afc2087
Mohammed Sami <mshelal555@gmail.com>
2022-10-19 20:06:05 +0200

    Add JPA annotations Closes #8

--- back end/src/main/java/com/spring/restuarant/model/BaseEntity.java
+++ back end/src/main/java/com/spring/restuarant/model/BaseEntity.java
@@ -4,9 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@MappedSuperclass
 public class BaseEntity {
+    @Id
+    @SequenceGenerator(name = "seq" , sequenceName = "sequence" , allocationSize = 1)
+    @GeneratedValue(generator = "sequence" , strategy = GenerationType.SEQUENCE)
     private Long id;
 }
--- back end/src/main/java/com/spring/restuarant/model/Order.java
+++ back end/src/main/java/com/spring/restuarant/model/Order.java
@@ -3,10 +3,14 @@ package com.spring.restuarant.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "order")
 public class Order extends BaseEntity {
     private String orderName;
     private int price;
