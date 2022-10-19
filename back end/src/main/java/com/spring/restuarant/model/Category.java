bd48bd7d87a891daad7baacb6a2a180d41836001
Mohammed Sami <mshelal555@gmail.com>
2022-10-19 19:31:51 +0200

    create model (Category & Order) Closes #6

---
+++ back end/src/main/java/com/spring/restuarant/model/Category.java
@@ -0,0 +1,6 @@
+package com.spring.restuarant.model;
+
+public class Category {
+    private Long id;
+    private String categoryName;
+}
---
+++ back end/src/main/java/com/spring/restuarant/model/Order.java
@@ -0,0 +1,9 @@
+package com.spring.restuarant.model;
+
+public class Order {
+    private Long id;
+    private String orderName;
+    private int price;
+    private String img;
+    private String description;
+}
